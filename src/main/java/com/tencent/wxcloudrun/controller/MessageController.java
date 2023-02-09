b0b2ba0e2bcedb495fea17dc701bc4a499237020
YoungLiuHappy <704253767@qq.com>
2023-02-09 20:13:06 +0800

    2023-2-9 20:13:04

--- src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
@@ -1,7 +1,5 @@
 package com.tencent.wxcloudrun.controller;
 
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 public class MessageController {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ApiResponse getMsg(@RequestBody JSONObject jsonObject) {
-		log.info("=收到消息Get={}",jsonObject);
+	public ApiResponse getMsg(String signature,String timestamp,String nonce,String echostr) {
+		log.info("=收到消息Get={}={}={}={}",signature,timestamp,nonce,echostr);
 		return ApiResponse.ok();
 	}
 	@RequestMapping(method = RequestMethod.POST)
-	public ApiResponse revMsg(@RequestBody JSONObject jsonObject) {
-		log.info("=收到消息POST={}",jsonObject);
+	public ApiResponse revMsg(String signature,String timestamp,String nonce,String echostr) {
+		log.info("=收到消息POST={}={}={}={}",signature,timestamp,nonce,echostr);
 		return ApiResponse.ok();
 	}
 }
