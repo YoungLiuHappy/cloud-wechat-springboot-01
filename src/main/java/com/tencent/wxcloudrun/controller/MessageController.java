7c3aa7db4c2f9ccdb8dff148abd26e385cae7210
YoungLiuHappy <704253767@qq.com>
2023-02-09 19:54:10 +0800

    2023-2-9 19:54:09

--- src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
@@ -1,25 +1,28 @@
 package com.tencent.wxcloudrun.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RestController("/api/message")
+@RestController
+@RequestMapping("/api/message")
 @Slf4j
 public class MessageController {
 	
-	@GetMapping
-	public ApiResponse getMsg(String msg) {
-		log.info("=收到消息Get={}",msg);
+	@RequestMapping(method = RequestMethod.GET)
+	public ApiResponse getMsg(@RequestBody JSONObject jsonObject) {
+		log.info("=收到消息Get={}",jsonObject);
 		return ApiResponse.ok();
 	}
-	@PostMapping
-	public ApiResponse revMsg(String msg) {
-		log.info("=收到消息POST={}",msg);
+	@RequestMapping(method = RequestMethod.POST)
+	public ApiResponse revMsg(@RequestBody JSONObject jsonObject) {
+		log.info("=收到消息POST={}",jsonObject);
 		return ApiResponse.ok();
 	}
 }
